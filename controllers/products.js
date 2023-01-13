9b45efecb057d9a4a6d3cd88c7236d0d8100e7f8
Neelabh Singh <neelabh041997@gmail.com>
2023-01-13 13:07:07 +0530

    Filtering based on name, featured and company

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,37 @@
+const Product = require('../models/product')
 const getAllProductsStatic = async (req, res, ) => {
-    // throw new Error('testing async errors')
-    res.status(200).json({msg: 'Products testing route'})
+    const search = 'ab'
+    const products = await Product.find({
+        name: {
+            $regex:search,
+            $options: 'i'
+        }
+    })
+    res.status(200).json({products, numberOfHits: products.length})
 }
 
 const getAllProducts = async (req, res, ) => {
-    res.status(200).json({msg: 'Products route'})
+    const {featured, company, name} = req.query
+    const queryObject = {}
+
+    if(featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    
+    if(company) {
+        queryObject.company = company
+    }
+    
+    if(name) {
+        queryObject.name = {
+            $regex: name,
+            $options: 'i'
+        }
+    }
+
+    console.log(queryObject)
+    const products = await Product.find(queryObject)
+    res.status(200).json({products, numberOfHits: products.length})
 }
 
 module.exports = {
